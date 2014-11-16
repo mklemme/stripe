62782cccdd0967b01748dd35c9fc72e41fb7b3a0
mklemme <mykklemme@gmail.com>
2014-11-16 13:33:29 -0800

    I combined the stripe update into the controller method. What happens next will shock you!

--- app/assets/javascripts/payment.js
+++ app/assets/javascripts/payment.js
@@ -1,35 +1,4 @@
-jQuery(function($) {
-  $('#payment-form').submit(function(event) {
-    var $form = $(this);
-
-    // Disable the submit button to prevent repeated clicks
-    $form.find('button').prop('disabled', true);
-
-    Stripe.card.createToken($form, stripeResponseHandler);
-
-    // Prevent the form from submitting with the default action
-    return false;
-  });
-});
-
-function stripeResponseHandler(status, response) {
-  var $form = $('#payment-form');
-
-  if (response.error) {
-    // Show the errors on the form
-    $form.find('.payment-errors').text(response.error.message);
-    $form.find('button').prop('disabled', false);
-  } else {
-    // response contains id and card, which contains additional card details
-    var token = response.id;
-    // Insert the token into the form so it gets submitted to the server
-    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-    // and submit
-    console.log("Doing something")
-    $form.get(0).submit();
-  }
-};
-$(function() {
+function engine(){
   $("#credit-card input, #credit-card select").attr("disabled", false);
 
   $("form:has(#credit-card)").submit(function() {
@@ -72,4 +41,78 @@ $(function() {
     $("#credit_card_number").focus();
     return false;
   });
-});
+
+  $('#payment-form').submit(function(event) {
+    var $form = $(this);
+
+    // Disable the submit button to prevent repeated clicks
+    $form.find('button').prop('disabled', true);
+
+    Stripe.card.createToken($form, stripeResponseHandler);
+
+    // Prevent the form from submitting with the default action
+    return false;
+  });
+}
+//
+// function stripeResponseHandler(status, response) {
+//   var $form = $('#payment-form');
+//
+//   if (response.error) {
+//     // Show the errors on the form
+//     $form.find('.payment-errors').text(response.error.message);
+//     $form.find('button').prop('disabled', false);
+//   } else {
+//     // response contains id and card, which contains additional card details
+//     var token = response.id;
+//     // Insert the token into the form so it gets submitted to the server
+//     $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+//     // and submit
+//     console.log("Doing something")
+//     $form.get(0).submit();
+//   }
+// };
+// $(function() {
+//   $("#credit-card input, #credit-card select").attr("disabled", false);
+//
+//   $("form:has(#credit-card)").submit(function() {
+//     var form = this;
+//     $("#user_submit").attr("disabled", true);
+//     $("#credit-card input, #credit-card select").attr("name", "");
+//     $("#credit-card-errors").hide();
+//
+//     if (!$("#credit-card").is(":visible")) {
+//       $("#credit-card input, #credit-card select").attr("disabled", true);
+//       return true;
+//     }
+//
+//     var card = {
+//       number:   $("#credit_card_number").val(),
+//       expMonth: $("#_expiry_date_2i").val(),
+//       expYear:  $("#_expiry_date_1i").val(),
+//       cvc:      $("#cvv").val()
+//     };
+//
+//
+//     Stripe.createToken(card, function(status, response) {
+//       if (status === 200) {
+//         $("#user_last_4").val(response.card.last4);
+//         $("#user_stripe_token").val(response.id);
+//         form.submit();
+//       } else {
+//         $("#stripe-error-message").text(response.error.message);
+//         $("#credit-card-errors").show();
+//         $("#user_submit").attr("disabled", false);
+//       }
+//     });
+//
+//     return false;
+//   });
+//
+//   $("#change-card a").click(function() {
+//     $("#change-card").hide();
+//     $("#credit-card").show();
+//     $("#credit_card_number").focus();
+//     return false;
+//   });
+// });
--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,36 +9,68 @@ class ChargesController < ApplicationController
     @user = current_user
     @trainer = Trainer.find_by_id(1)
     @amount = params[:charge_amount]
-    # Set your secret key: remember to change this to your live secret key in production
-    # See your keys here https://dashboard.stripe.com/account
+    last_4_digits = params[:last_4]
+    charge_amount = (params[:charge_amount].to_d * 100).to_i
 
     Stripe.api_key = Figaro.env.stripe_api_key
+
+    if params[:charge_amount] == ""
+      render "new", error: "You focking wot m8?"
+    end
 
     # Get the credit card details submitted by the form
     if @user.stripe_id?
       token = @user.stripe_id
+      customer = Stripe::Customer.retrieve(current_user.stripe_id)
+      customer.email = current_user.email
+      customer.save
+
+      begin
+        charge = Stripe::Charge.create(
+          :amount => charge_amount, # amount in cents, again
+          :currency => "usd",
+          :customer => token,
+          :description => @user.email
+        )
+
+        rescue Stripe::CardError => e
+          flash[:error] = e.message
+          redirect_to event_path(@event)
+
+      end
+
+      redirect_to root_path, notice: "Your new card was saved and charged for #{@amount}"
+
+    # If customer hasn't been created
     else
+
       token = params[:stripe_token]
+
+      customer = Stripe::Customer.create(
+        :email => current_user.email,
+        :card => stripe_token,
+        :description => current_user.email
+      )
+
+      charge = Stripe::Charge.create(
+        :amount => charge_amount, # amount in cents, again
+        :currency => "usd",
+        :card => token,
+        :description => current_user.email
+      )
+
+      current_user.stripe_id = customer.id
+      current_user.last_4_digits = last_4_digits
+      current_user.save
+      redirect_to root_path, notice: "Your Stripe account was create. Plus, we saved and charged your new card!"
     end
 
-    charge_amount = (params[:charge_amount].to_d * 100).to_i
+
 
 
     # charge_amount = view_context.number_with_precision(params[:charge_amount], :precision => 2) * 100
     # Create the charge on Stripe's servers - this will charge the user's card
-    begin
-      charge = Stripe::Charge.create(
-        :amount => charge_amount, # amount in cents, again
-        :currency => "usd",
-        :customer => token,
-        :description => @user.email
-      )
 
-      rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to event_path(@event)
-    end
-    redirect_to root_path, notice: "OK"
   end
 
   private
